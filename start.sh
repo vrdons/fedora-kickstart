59528313c31f54d205cece83ca64e8eea5a87859
vrdons <vrdons@proton.me>
2025-12-26 16:55:28 +0300

    fix: for next process

--- start.sh
+++ start.sh
@@ -3,6 +3,7 @@ sudo mount -o loop Fedora-43-Server.iso /mnt
 
 # Start http server to see install.ks
 python3 -m http.server 8000 &
+HTTP_PID=$!
 
 # Start qemu
 qemu-system-x86_64 \
@@ -15,3 +16,9 @@ qemu-system-x86_64 \
 	-initrd /mnt/images/pxeboot/initrd.img \
 	-append "inst.stage2=cdrom inst.ks=http://10.0.2.2:8000/install.ks" \
 	-boot d
+
+# Unmount /mnt for next process
+sudo umount /mnt
+
+# Kill http server for next process
+kill $HTTP_PID
